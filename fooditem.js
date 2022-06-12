33a1a768f840a6cd05a9a29d7ea4ed268322c467
Prateek Saxena <102294280+Prateeksaxena6@users.noreply.github.com>
2022-06-13 01:22:42 +0530

    Add files via upload

---
+++ fooditem.js
@@ -0,0 +1,188 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Veg Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 100,
+    img: 'pictures/biryani/veg biryani.jpg',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'Hyderabadi Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 100,
+    img: 'pictures/biryani/Hyderabadi dum biryani.jpg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Veg Pulav',
+    category : 'biryani',
+    rating : 4.3,
+    price: 90,
+    img: 'pictures/biryani/veg pulao.jpg',
+    quantity: 1
+},
+
+{
+    id: 5,
+    name: 'Fried Rice',
+    category : 'biryani',
+    rating : 4.3,
+    price: 80,
+    img: 'pictures/biryani/vegetarian fried Rice.jpg',
+    quantity: 1
+},
+
+{
+    id: 7,
+    name: 'Kashmiri Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 130,
+    img: 'pictures/biryani/kashmiri biryani.jpg',
+    quantity: 1
+},
+{
+    id: 16,
+    name: 'Matar Paneer',
+    category : 'paneer',
+    rating : 4.3,
+    price: 50,
+    img: 'pictures/North indian/matar paneer.jpg',
+    quantity: 1
+
+},
+{
+    id: 17,
+    name: 'Kadai Paneer',
+    category : 'paneer',
+    rating : 4.3,
+    price: 60,
+    img: 'pictures/North indian/kadai paneer.jpg',
+    quantity: 1
+},
+{
+    id: 18,
+    name: 'Shahi Paneer',
+    category : 'paneer',
+    rating : 4.3,
+    price: 80,
+    img: 'pictures/North indian/shahi paneer.jpg',
+    quantity: 1
+
+},
+{
+    id: 19,
+    name: 'Palak Paneer',
+    category : 'paneer',
+    rating : 4.3,
+    price: 70,
+    img: 'pictures/North indian/palak paneer.jpg',
+    quantity: 1
+
+},
+
+
+{
+    id: 23,
+    name: 'Amritsari naan',
+    category : 'pure veg',
+    rating : 4.3,
+    price: 50,
+    img: 'pictures/North indian/amritsari naan.jpg',
+    quantity: 1
+},
+{
+    id: 24,
+    name: 'Bread Pakora',
+    category : 'pure veg',
+    rating : 4.3,
+    price: 25,
+    img: 'pictures/North indian/bread pakora.jpg',
+    quantity: 1
+},
+{
+    id: 25,
+    name: 'Chole kulche',
+    category : 'pure veg',
+    rating : 4.3,
+    price: 30,
+    img: 'pictures/North indian/chole kulche.jpg',
+    quantity: 1
+},
+{
+    id: 26,
+    name: 'Veg Curry',
+    category : 'pure veg',
+    rating : 4.3,
+    price: 70,
+    img: 'pictures/North indian/veg curry.jpg',
+    quantity: 1
+},
+{
+    id: 27,
+    name: 'chole bhature',
+    category : 'pure veg',
+    rating : 4.3,
+    price: 30,
+    img: 'pictures/North indian/chole bhature.jpeg',
+    quantity: 1
+},
+
+
+{
+    id: 37,
+    name: ' Masala Dosa',
+    category : 'south indian',
+    rating : 4.3,
+    price: 60,
+    img: 'pictures/south indian/masala dosa.jpg',
+    quantity: 1
+},
+{
+    id: 38,
+    name: 'Idli sambhar',
+    category : 'south indian',
+    rating : 4.3,
+    price: 40,
+    img: 'pictures/south indian/idli sambhar.jpg',
+    quantity: 1
+},
+
+
+{
+    id: 41,
+    name: 'Onion Uttapam',
+    category : 'south indian',
+    rating : 4.3,
+    price: 70,
+    img: 'pictures/south indian/onion Uttapam.jpg',
+    quantity: 1
+},
+
+
+{
+    id: 44,
+    name: 'Sambhar Vada',
+    category : 'south indian',
+    rating : 4.3,
+    price: 30,
+    img: 'pictures/south indian/sambhar vada.jpg',
+    quantity: 1
+},
+{
+    id: 42,
+    name: 'Plain Dosa',
+    category : 'south indian',
+    rating : 4.3,
+    price: 40,
+    img: 'pictures/south indian/plain dosa.jpg',
+    quantity: 1
+},
+]
+
+export {foodItem};
